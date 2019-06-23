692cccf88bacfc643b60ce3b80328a0ef14f81c1
saki0208 <sakura1104usa@gmail.com>
2019-06-23 20:16:18 +0900

    変更６月23日

--- app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
@@ -2,103 +2,66 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 public class DisplayMessageActivity extends AppCompatActivity {
+     BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        setContentView(R.layout.activity_main);
+
+
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        if((message).equals("0000")) {
+        if ((message).equals("0000")) {
             // Capture the layout's TextView and set the string as its text
-            TextView textView = findViewById(R.id.textView);
-            textView.setText("success");
+
+            barChart = findViewById(R.id.mp_BarChart);
+            BarDataSet barDataSet1 = new BarDataSet(dataValuesl(), "DataSet 1");
+            BarData barData =new BarData();
+            barData.addDataSet(barDataSet1);
+            barChart.setData(barData);
+            barChart.invalidate();
 
 
 
+        } else {
 
-
-        }else{
-            TextView textView = findViewById(R.id.textView);
-            textView.setText("error");
         }
     }
-    protected void createBarChart() {
-        BarChart barChart = (BarChart) findViewById(R.id.bar_chart);
-        barChart.setDescription("BarChart 説明");
+        private ArrayList<BarEntry> dataValuesl(){
+            ArrayList<BarEntry> dataVals =new ArrayList<>();
+            dataVals.add(new BarEntry(0,3));
+            dataVals.add(new BarEntry(1,4));
+            dataVals.add(new BarEntry(3,6));
+            dataVals.add(new BarEntry(2,7));
+            dataVals.add(new BarEntry(4,11));
+            return dataVals;
+        }
 
-        barChart.getAxisRight().setEnabled(false);
-        barChart.getAxisLeft().setEnabled(true);
-        barChart.setDrawGridBackground(true);
-        barChart.setDrawBarShadow(false);
-        barChart.setEnabled(true);
 
-        barChart.setTouchEnabled(true);
-        barChart.setPinchZoom(true);
-        barChart.setDoubleTapToZoomEnabled(true);
 
-        barChart.setHighlightEnabled(true);
-        barChart.setDrawHighlightArrow(true);
-        barChart.setHighlightEnabled(true);
 
-        barChart.setScaleEnabled(true);
-
-        barChart.getLegend().setEnabled(true);
-
-        //X軸周り
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setDrawLabels(true);
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setDrawGridLines(true);
-        xAxis.setSpaceBetweenLabels(0);
-
-        barChart.setData(createBarChartData());
-
-        barChart.invalidate();
-        // アニメーション
-        barChart.animateY(2000, Easing.EasingOption.EaseInBack);
     }
 
-    // BarChartの設定
-    private BarData createBarChartData() {
-        ArrayList<BarDataSet> barDataSets = new ArrayList<>();
-
-        // X軸
-        ArrayList<String> xValues = new ArrayList<>();
-        xValues.add("1月");
-        xValues.add("2月");
 
 
-        // valueA
-        ArrayList<BarEntry> valuesA = new ArrayList<>();
-        valuesA.add(new BarEntry(100, 0));
-        valuesA.add(new BarEntry(200, 1));
 
-        BarDataSet valuesADataSet = new BarDataSet(valuesA, "A");
-        valuesADataSet.setColor(ColorTemplate.COLORFUL_COLORS[3]);
 
-        barDataSets.add(valuesADataSet);
 
-        // valueB
-        ArrayList<BarEntry> valuesB = new ArrayList<>();
-        valuesB.add(new BarEntry(200, 0));
-        valuesB.add(new BarEntry(300, 1));
-
-        BarDataSet valuesBDataSet = new BarDataSet(valuesB, "B");
-        valuesBDataSet.setColor(ColorTemplate.COLORFUL_COLORS[4]);
-
-        barDataSets.add(valuesBDataSet);
-
-        BarData barData = new BarData(xValues, barDataSets);
-        return barData;
-    }
-
-}
